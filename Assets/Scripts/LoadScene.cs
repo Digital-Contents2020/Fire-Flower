a4aeef260baeff9a0ff94f84da06fcebb721143d
myamano <nbz.orenge@gmail.com>
2020-12-16 05:50:54 +0900

    add:scenemanager

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -3,11 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    string scene_Login = "Login"; 
-    string scene_Main = "Main"; 
-    string scene_End = "End"; 
- 
-    public static void ChangeScene(){ 
+       public static void ChangeScene(){ 
         Debug.Log(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name); 
         if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Login"){ 
             UnityEngine.SceneManagement.SceneManager.LoadScene("Main"); 
