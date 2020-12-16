ae79ebdd30c6873a9d3e7f6d13816855799c63ed
myamano <nbz.orenge@gmail.com>
2020-12-17 00:47:00 +0900

    add : mobile keyboard

---
+++ Assets/Scripts/TouchKeyBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchKeyBoard : MonoBehaviour
+{
+    public string stringToEdit = "Hello World!";
+
+    [SerializeField] Text text;
+
+    private TouchScreenKeyboard keyboard;
+
+    private void Update() {
+        if(keyboard != null && keyboard.status == TouchScreenKeyboard.Status.Done){
+
+        }
+    }
+
+    public void OnButtonClicked(){
+        keyboard = TouchScreenKeyboard.Open("Enter Your Name");
+
+        keyboard.text = "なぎさああああああ";
+    }
+}
