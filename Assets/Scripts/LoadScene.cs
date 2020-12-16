091681e0f4db6863f03553bc4f5717fa080d7032
myamano <nbz.orenge@gmail.com>
2020-12-17 05:03:15 +0900

    update: Start Scene

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,32 +1,31 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LoadScene : MonoBehaviour
-{
+public class LoadScene : MonoBehaviour {
     [SerializeField] InputField PlayerNameInput = default;
 
-    private void Start() {
-        if(PlayerNameInput != null){
-            PlayerNameInput.text = "Player " + Random.Range(1000, 10000);
+    private void Start () {
+        if (PlayerNameInput != null) {
+            PlayerNameInput.text = "Player " + Random.Range (1000, 10000);
         }
     }
 
-    public void ChangeScene(){ 
+    public void ChangeScene () {
 
         //スタート画面からメイン画面への遷移
-        if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Login"){ 
-            SceneManager.instance.OnLoginButtonClicked(PlayerNameInput.text);
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Main"); 
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "Start") {
+            SceneManager.instance.OnLoginButtonClicked (PlayerNameInput.text);
+            UnityEngine.SceneManagement.SceneManager.LoadScene ("Main");
         }
         //メイン画面から終了画面への遷移
-        else if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Main"){ 
-            SceneManager.instance.OnEndButtonClicked();
-            UnityEngine.SceneManagement.SceneManager.LoadScene("End"); 
-        } 
+        else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "Main") {
+            SceneManager.instance.OnEndButtonClicked ();
+            UnityEngine.SceneManagement.SceneManager.LoadScene ("End");
+        }
         //終了画面からスタート画面への遷移
-        else if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "End"){ 
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Login"); 
-        } 
+        else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene ().name == "End") {
+            UnityEngine.SceneManagement.SceneManager.LoadScene ("Start");
+        }
     }
 }
