c0ef5517cd5d1df59b2bff6415331e29f2173948
myamano <nbz.orenge@gmail.com>
2020-12-17 17:27:06 +0900

    merge

--- Assets/UI/Script/Sound/audio.cs
+++ Assets/UI/Script/Sound/audio.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 //Sound_stopサウンドの再生と停止ボタンpart1
@@ -8,15 +6,12 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class audio : MonoBehaviour
 {
-    
     public Sprite spritePlay;
     public Sprite spriteStop;
 
     Image image;
     AudioSource BGM;
     void Start() {
-                    // b1.SetActive(false);
-                    //b1=GetComponent<Button>();//buttonコンポネ取得
         image = GetComponent<Image>();
         BGM = GetComponent<AudioSource>();
     }
@@ -31,10 +26,6 @@ public class audio : MonoBehaviour
             BGM.Play();
             image.sprite = spritePlay;
         }
-        // BGM.Stop(); //audioを止める
         Debug.Log ("clicked");
-        // this.gameObject.SetActive(false);//非アクティブ化
-        //b1.SetActive(true);
-        // b1.SetActive(true);
     }
 }
